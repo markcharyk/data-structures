84795bd6ec3b9808f7e6cb086b242fc83d723e78
markcharyk <markcharyk@gmail.com>
2014-03-10 15:55:01 -0700

    Add binary tree

---
+++ data_structures/bst.py
@@ -0,0 +1,114 @@
+import random
+import time
+
+
+class BSTree(object):
+    def __init__(self, *val):
+        """Initialize a "leaf" of the tree to have a None value"""
+        if val != ():
+            self.val = val[0]
+            self.left = BSTree()
+            self.right = BSTree()
+        else:
+            self.val = None
+            self.left = None
+            self.right = None
+
+    def __str__(self):
+        """Return a string of all of the elements in the
+        tree and its decendents in order"""
+        result = ''
+        if self.left.val is not None:
+            result += str(self.left)
+        result += "%s " % str(self.val)
+        if self.right.val is not None:
+            result += str(self.right)
+        return result
+
+    def insert(self, val):
+        """Insert a value below the root of the tree"""
+        if self.val is None:
+            self.__init__(val)
+        elif self.val > val:
+            self.left.insert(val)
+        elif self.val < val:
+            self.right.insert(val)
+
+    def contains(self, val):
+        """Return whether the item is contained in the tree"""
+        if self.val is None:
+            return False
+        elif self.val == val:
+            return True
+        elif self.val > val:
+            return self.left.contains(val)
+        return self.right.contains(val)
+
+    def size(self):
+        """Return the size of the tree"""
+        count = 0
+        if self.val is None:
+            return count
+        else:
+            count += 1
+        count += self.left.size()
+        count += self.right.size()
+        return count
+
+    def depth(self):
+        """Return the maximum depth of the tree"""
+        result = 0
+        if self.val is None:
+            return result
+        else:
+            return max(self.left.depth() + 1, self.right.depth() + 1)
+
+    def balance(self):
+        """Return a value representing the balance of the tree"""
+        if self.val is None:
+            return 0
+        else:
+            return self.left.size() - self.right.size()
+
+    def get_dot(self):
+        """return the tree with root 'self' as a dot graph for visualization"""
+        return "digraph G{\n%s}" % ("" if self.val is None else (
+            "\t%s;\n%s\n" % (
+                self.val,
+                "\n".join(self._get_dot())
+            )
+        ))
+
+    def _get_dot(self):
+        """recursively prepare a dot graph entry for this node."""
+        if self.left is not None:
+            yield "\t%s -> %s;" % (self.val, self.left.val)
+            for i in self.left._get_dot():
+                yield i
+        elif self.right is not None:
+            r = random.randint(0, 1e9)
+            yield "\tnull%s [shape=point];" % r
+            yield "\t%s -> null%s;" % (self.val, r)
+        if self.right is not None:
+            yield "\t%s -> %s;" % (self.val, self.right.val)
+            for i in self.right._get_dot():
+                yield i
+        elif self.left is not None:
+            r = random.randint(0, 1e9)
+            yield "\tnull%s [shape=point];" % r
+            yield "\t%s -> null%s;" % (self.val, r)
+
+
+if __name__ == '__main__':
+    b1 = BSTree()
+    b2 = BSTree()
+    for i in xrange(512):
+        b1.insert(random.choice(range(4096)))
+        b2.insert(i)
+    t1 = time.time()
+    is_in_b1 = b1.contains(500)
+    t2 = time.time()
+    is_in_b2 = b2.contains(500)
+    t3 = time.time()
+    print "Random case: %f seconds" % (t2 - t1)
+    print "Worst case: %f seconds" % (t3 - t2)
---
+++ tests/bst_tests.py
@@ -0,0 +1,165 @@
+import unittest
+from data_structures.bst import BSTree
+
+
+class testInsert(unittest.TestCase):
+    def setUp(self):
+        self.bst = BSTree()
+
+    def testEmpty(self):
+        self.assertEqual(self.bst.val, None)
+        self.bst.insert(2)
+        self.assertEqual(self.bst.val, 2)
+
+    def testOne(self):
+        self.bst.insert(2)
+        self.assertEqual(self.bst.left.val, None)
+        self.assertEqual(self.bst.right.val, None)
+        self.bst.insert(5)
+        self.assertEqual(self.bst.left.val, None)
+        self.assertEqual(self.bst.right.val, 5)
+
+    def testFull(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.right.left.left.val, None)
+        self.assertEqual(self.bst.right.left.right.val, None)
+        self.bst.insert(4)
+        self.assertEqual(self.bst.right.left.left.val, None)
+        self.assertEqual(self.bst.right.left.right.val, 4)
+
+    def testFullRepeat(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.right.left.left.val, None)
+        self.assertEqual(self.bst.right.left.right.val, None)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.right.left.left.val, None)
+        self.assertEqual(self.bst.right.left.right.val, None)
+
+
+class testContains(unittest.TestCase):
+    def setUp(self):
+        self.bst = BSTree()
+
+    def testEmpty(self):
+        self.assertFalse(self.bst.contains(4))
+
+    def testOne(self):
+        self.bst.insert(2)
+        self.assertTrue(self.bst.contains(2))
+        self.assertFalse(self.bst.contains(4))
+
+    def testFull(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertTrue(self.bst.contains(3))
+        self.assertFalse(self.bst.contains(4))
+
+
+class testSize(unittest.TestCase):
+    def setUp(self):
+        self.bst = BSTree()
+
+    def testEmpty(self):
+        self.assertEqual(self.bst.size(), 0)
+
+    def testOne(self):
+        self.bst.insert(2)
+        self.assertEqual(self.bst.size(), 1)
+
+    def testFull(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.size(), 5)
+
+    def testFullRepeat(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.size(), 5)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.size(), 5)
+
+
+class testDepth(unittest.TestCase):
+    def setUp(self):
+        self.bst = BSTree()
+
+    def testEmpty(self):
+        self.assertEqual(self.bst.depth(), 0)
+
+    def testOne(self):
+        self.bst.insert(2)
+        self.assertEqual(self.bst.depth(), 1)
+
+    def testFull(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.depth(), 3)
+
+    def testFullRepeat(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.depth(), 3)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.depth(), 3)
+
+
+class testBalance(unittest.TestCase):
+    def setUp(self):
+        self.bst = BSTree()
+
+    def testEmpty(self):
+        self.assertEqual(self.bst.balance(), 0)
+
+    def testOne(self):
+        self.assertEqual(self.bst.balance(), 0)
+
+    def testFullRight(self):
+        self.bst.insert(2)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.balance(), -2)
+
+    def testFullLeft(self):
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(2)
+        self.bst.insert(8)
+        self.bst.insert(3)
+        self.assertEqual(self.bst.balance(), 2)
+
+    def testFullEven(self):
+        self.bst.insert(3)
+        self.bst.insert(5)
+        self.bst.insert(1)
+        self.bst.insert(2)
+        self.bst.insert(8)
+        self.assertEqual(self.bst.balance(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
