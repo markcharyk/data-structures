1beea6c76045563d1697ff199ad0f7f8efa8f048
markcharyk <markcharyk@gmail.com>
2014-04-13 19:08:41 -0700

    Add upheap tests

--- tests/heap_tests.py
+++ tests/heap_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from data_structures.heap import Heap
+from data_structures.heap import Heap, Node
 
 
 class TestInsert(unittest.TestCase):
@@ -8,36 +8,36 @@ class TestInsert(unittest.TestCase):
 
     def test_empty(self):
         self.h.insert(5, 'Five')
-        self.assertEqual(self.h[0].val, 'Five')
+        self.assertEqual(self.h.heap[0].val, 'Five')
 
     def test_left(self):
         self.h.insert(5, 'Five')
         self.h.insert(8, 'Eight')
-        self.assertEqual(self.h[1].val, 'Eight')
+        self.assertEqual(self.h.heap[1].val, 'Eight')
 
     def test_right(self):
         self.h.insert(5, 'Five')
         self.h.insert(8, 'Eight')
         self.h.insert(10, 'Ten')
-        self.assertEqual(self.h[2].val, 'Ten')
+        self.assertEqual(self.h.heap[2].val, 'Ten')
 
     def test_new_min(self):
         self.h.insert(5, 'Five')
         self.h.insert(8, 'Eight')
         self.h.insert(10, 'Ten')
         self.h.insert(3, 'Three')
-        self.assertEqual(self.h[0].val, 'Three')
-        self.assertEqual(self.h[1].val, 'Five')
-        self.assertEqual(self.h[2].val, 'Ten')
-        self.assertequal(self.h[3].val, 'Eight')
+        self.assertEqual(self.h.heap[0].val, 'Three')
+        self.assertEqual(self.h.heap[1].val, 'Five')
+        self.assertEqual(self.h.heap[2].val, 'Ten')
+        self.assertequal(self.h.heap[3].val, 'Eight')
 
     def test_dupe(self):
         self.h.insert(5, 'Five')
         self.h.insert(8, 'Eight')
         self.h.insert(5, 'Five')
-        self.assertEqual(self.h[0].val, 'Five')
-        self.assertEqual(self.h[1].val, 'Eight')
-        self.assertEqual(self.h[2].val, 'Five')
+        self.assertEqual(self.h.heap[0].val, 'Five')
+        self.assertEqual(self.h.heap[1].val, 'Eight')
+        self.assertEqual(self.h.heap[2].val, 'Five')
 
     def test_middle(self):
         self.h.insert(3, 'Three')
@@ -48,13 +48,40 @@ class TestInsert(unittest.TestCase):
         self.h.insert(11, 'Eleven')
         self.h.insert(12, 'Twelve')
         self.h.insert(4, 'Four')
-        self.assertEqual(self.h[0].val, 'Three')
-        self.assertEqual(self.h[1].val, 'Four')
-        self.assertEqual(self.h[3].val, 'Five')
+        self.assertEqual(self.h.heap[0].val, 'Three')
+        self.assertEqual(self.h.heap[1].val, 'Four')
+        self.assertEqual(self.h.heap[3].val, 'Five')
 
 
 class TestUpheap(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.h = Heap()
+        self.h.insert(5, 'Five')
+
+    def test_once_left(self):
+        self.h.heap.append(Node(3, 'Three'))
+        self.h._upheap(1)
+        self.assertEqual(self.h.heap[0].val, 'Three')
+        self.assertEqual(self.h.heap[1].val, 'Five')
+
+    def test_once_right(self):
+        self.h.insert(8, 'Eight')
+        self.h.heap.append(Node(3, 'Three'))
+        self.h._upheap(2)
+        self.assertEqual(self.h.heap[0].val, 'Three')
+        self.assertEqual(self.h.heap[2].val, 'Five')
+
+    def test_many(self):
+        self.h.insert(3, 'Three')
+        self.h.insert(8, 'Eight')
+        self.h.insert(10, 'Ten')
+        self.h.insert(13, 'Thirteen')
+        self.h.insert(11, 'Eleven')
+        self.h.insert(12, 'Twelve')
+        self.h.insert(4, 'Four')
+        self.h.heap.append(Node(2, 'Two'))
+        self.h._upheap(len(self.h.heap) - 1)
+        self.assertEqual(self.h.heap[0].val, 'Two')
 
 
 class TestDeleteMin(unittest.TestCase):
@@ -69,26 +96,26 @@ class TestDeleteMin(unittest.TestCase):
     def test_left(self):
         self.h.insert(8, 'Eight')
         self.assertEqual(self.h.delete_min().val, 'Five')
-        self.assertEqual(self.h[0].val, 'Eight')
+        self.assertEqual(self.h.heap[0].val, 'Eight')
 
     def test_right(self):
         self.h.insert(8, 'Eight')
         self.h.insert(10, 'Ten')
         self.assertEqual(self.h.delete_min().val, 'Five')
-        self.assertEqual(self.h[0].val, 'Eight')
-        self.assertEqual(self.h[1].val, 'Ten')
+        self.assertEqual(self.h.heap[0].val, 'Eight')
+        self.assertEqual(self.h.heap[1].val, 'Ten')
 
     def test_dupe(self):
         self.h.insert(5, 'Five')
         self.assertEqual(self.h.delete_min().val, 'Five')
-        self.assertEqual(self.h[0].val, 'Five')
+        self.assertEqual(self.h.heap[0].val, 'Five')
 
     def test_new_min(self):
         self.h.insert(8, 'Eight')
         self.h.insert(10, 'Ten')
         self.h.insert(3, 'Three')
         self.assertEqual(self.h.delete_min().val, 'Three')
-        self.assertEqual(self.h[0].val, 'Five')
+        self.assertEqual(self.h.heap[0].val, 'Five')
 
     def test_multiple(self):
         self.h.insert(3, 'Three')
