3c5c28187a6d4906c4b3067b49392dcd8911600d
markcharyk <markcharyk@gmail.com>
2014-04-14 11:14:28 -0700

    Add insert + delete

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -9,13 +9,15 @@ class Heap(object):
         self.heap = []
 
     def insert(self, key, val):
-        pass
+        self.heap.append(Node(key, val))
+        self._upheap(len(self.heap) - 1)
 
     def _upheap(self, idx):
         pass
 
     def delete_min(self):
-        pass
+        if self.heap:
+            return self.heap.pop().val
 
     def _downheap(self, idx):
         pass
