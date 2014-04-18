35ff164b9c8b381f1b24a9c6dfb8a434489041bd
markcharyk <markcharyk@gmail.com>
2014-04-18 16:17:16 -0700

    Add tree insert tests

--- tests/b_tree_tests.py
+++ tests/b_tree_tests.py
@@ -176,19 +176,47 @@ class TestInsertTree(unittest.TestCase):
         self.b = BTree()
 
     def test_empty(self):
-        pass
+        self.b.insert(5, 'Five')
+        self.assertEqual(self.b.root.elems[0][1], 'Five')
 
     def test_at_root(self):
-        pass
-
-    def test_no_split(self):
-        pass
-
-    def test_split_node(self):
-        pass
+        self.b.insert(5, 'Five')
+        self.b.insert(4, 'Four')
+        self.assertEqual(self.b.root.elems[0][1], 'Four')
 
     def test_new_level(self):
-        pass
+        self.b.insert(4, 'Four')
+        self.b.insert(6, 'Six')
+        self.b.insert(5, 'Five')
+        self.assertEqual(self.b.root.elems[0][1], 'Five')
+        self.assertIsNone(self.b.root.elems[1][1])
+        self.assertEqual(self.b.root.left.elems[0][1], 'Four')
+        self.assertIsNone(self.b.root.left.elems[1][1])
+        self.assertEqual(self.b.root.right.elems[0][1], 'Six')
+        self.assertIsNone(self.b.root.right.elems[1][1])
+
+    def test_no_split(self):
+        self.b.insert(4, 'Four')
+        self.b.insert(2, 'Two')
+        self.b.insert(6, 'Six')
+        self.b.insert(8, 'Eight')
+        self.b.insert(1, 'One')
+        self.assertEqual(self.b.root.left.elems[0][1], 'One')
+        self.assertEqual(self.b.root.right.elems[1][1], 'Eight')
+
+    def test_split_node(self):
+        self.b.insert(4, 'Four')
+        self.b.insert(2, 'Two')
+        self.b.insert(6, 'Six')
+        self.b.insert(8, 'Eight')
+        self.b.insert(1, 'One')
+        self.b.insert(7, 'Seven')
+        self.assertEqual(self.b.root.elems[0][1], 'Four')
+        self.assertEqual(self.b.root.elems[1][1], 'Seven')
+        self.assertEqual(self.b.root.mid.elems[0][1], 'Six')
+        self.assertIsNone(self.b.root.mid.elems[1][1])
+        self.assertEqual(self.b.root.right.elems[0][1], 'Eight')
+        self.assertIsNone(self.b.root.right.elems[1][1])
 
 
 class TestDeleteTree(unittest.TestCase):
